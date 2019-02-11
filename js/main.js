24f98db245aac823492e42ef0f12abd3d255b259
Senthilkumar S <senthilkumar.snp@gmail.com>
2019-02-11 21:47:39 +0530

    First commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+
+
+$(document).ready(function(){
+
+$(".color-picker-group .dropdown-menu").mCustomScrollbar({
+      callbacks:{
+        whileScrolling:function(){
+            $('body').addClass('stop-pagescroll');
+        },
+        onScroll: function(){
+          setTimeout(function(){
+            $('body').removeClass('stop-pagescroll');
+          },200);
+        },
+        onScrollStart:function(){
+            $('body').addClass('stop-pagescroll');
+        },
+      }
+  });
+
+  $('.color-picker-group input').on('click', function(){
+
+    $.ajax({
+      type: 'get',
+      url: 'js/colors.json',
+      success: function (response) {
+
+        var resArray = response;
+
+        $.each(resArray, function(i, color){
+            $('.color-picker-group .dropdown-menu ul').append('<li class="list-inline-item" data-class="'+resArray[i].name+'" data-color="'+resArray[i].hexString+'"  style="background:'+resArray[i].hexString+'"></li>');
+        });
+
+      }
+
+    });
+
+  });
+
+  $(document).on('click', '.color-picker-group .dropdown-menu li', function(e){
+    e.stopPropagation();
+  });
+
+  $(document).on('click', '.color-picker-group .dropdown-menu li', function(){
+    var colorValue = $(this).attr('data-color');
+    $('.color-picker-group input').css('background',colorValue);
+
+  });
+
+
+
+});
